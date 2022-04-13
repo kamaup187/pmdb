882fa3cf59657b44b28779ebd9e4531ebcd6c1d6
koechpetersn <koechpetersn@gmail.com>
2022-04-13 08:35:48 +0300

    bugfix

--- app/v1/views/apartment_management.py
+++ app/v1/views/apartment_management.py
@@ -120,8 +120,12 @@ class Index(Resource):
         #     ApartmentOp.update_reminder_status(qqq,"pending")
         #     print("Update complete",qqq, "now",qqq.reminder_status)
 
-
         time = datetime.datetime.now() + relativedelta(hours=3)
+
+        cooo = CompanyOp.fetch_company_by_id(16)
+        if cooo:
+            print("ccccccccccccccoooooo",cooo.name)
+            # CompanyOp.delete(cooo)
 
         # ctob_obj = CtoBop("MQCKMSCNS",time,50.0,"trans_type","400400","C5","invoice_num","0716674695",786.0,"Peter","Koech")
         # ctob_obj.save()

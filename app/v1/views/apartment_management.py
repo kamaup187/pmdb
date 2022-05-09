121bc475202b562bc86c53187d59a207e609f48b
Koech Peter <koechpetersn@gmail.com>
2022-05-09 13:41:43 +0300

    update

--- app/v1/views/apartment_management.py
+++ app/v1/views/apartment_management.py
@@ -111,13 +111,13 @@ class Index(Resource):
     def get(self):
 
 
-        qws = ApartmentOp.fetch_apartment_by_id(421)
+        # qws = ApartmentOp.fetch_apartment_by_id(421)
 
-        if qws:
-            all_ptenants = qws.meters
-            decitype = "1"
-            for i in all_ptenants:
-                MeterOp.update_decitype(i,decitype)
+        # if qws:
+        #     all_ptenants = qws.meters
+        #     decitype = "1"
+        #     for i in all_ptenants:
+        #         MeterOp.update_decitype(i,decitype)
 
         # from rq import cancel_job
         # cancel_job('3771ae2a-e121-4834-af5a-1c61e04b5b08')

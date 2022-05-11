3dd9cfeb7d70b2e81e3a46346c3df0182cb792ac
Koech Peter <koechpetersn@gmail.com>
2022-05-11 12:52:47 +0300

    update

--- app/v1/views/apartment_management.py
+++ app/v1/views/apartment_management.py
@@ -410,7 +410,10 @@ class Index(Resource):
             if company.name == "Lesama Ltd":
                 shortcode = "Paybill: 969610 Acc: LesamaKe"
             else:
-                shortcode = f"Paybill: 4081687 Acc: {companyname2}#{company.id}"
+                if os.getenv("TARGET") == "lasshouse":
+                    shortcode = f"Paybill: 000XXX Acc: {companyname2}#{company.id}"
+                else:
+                    shortcode = f"Paybill: 4081687 Acc: {companyname2}#{company.id}"
 
             indexpage = "agentindex2.html" if os.getenv("TARGET") == "lasshouse" else "agentindex.html"
 

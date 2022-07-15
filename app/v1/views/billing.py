1376666f02d42075a23a3c9720899890e8f42581
Koech Peter <koechpetersn@gmail.com>
2022-07-15 13:36:11 +0300

    update

--- app/v1/views/billing.py
+++ app/v1/views/billing.py
@@ -2288,7 +2288,7 @@ class ReceivePayment(Resource):
                         cbid_id = cb.bill_ref_num.replace(" ","")
                         if "-" in cbid_id:
                             cbid_id2 = cbid_id.split("-")[1]
-                            print(cbid_id2)
+                            print(cbid_id2,"length",len(cbid_id2))
                         else:
                             print("maintained cbid")
                             cbid_id2 = cbid_id

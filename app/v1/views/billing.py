eaa8d63602b74519dab60bcfb4cc3b136ac0e352
Koech Peter <koechpetersn@gmail.com>
2022-08-01 16:27:14 +0300

    update

--- app/v1/views/billing.py
+++ app/v1/views/billing.py
@@ -4483,6 +4483,40 @@ class CallBackUrlBizlineNeema(Resource):
 
         mpesa_response(ctob_obj)
 
+class CallBackUrlLagad(Resource):
+    def get(self):
+        pass
+    def post(self):
+        #parse for json
+        my_data=request.data
+        my_json = my_data.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
+
+        trans_id = data['TransID']
+        trans_time = data['TransTime']
+        trans_amnt = data['TransAmount']
+        trans_type = data['TransactionType']
+        business_shortcode = data['BusinessShortCode']
+        bill_ref_num = data['BillRefNumber']
+        invoice_num = data['InvoiceNumber']
+        msisdn = data['MSISDN']
+        org_acc_bal = data['OrgAccountBalance']
+        fname = data['FirstName']
+        try:
+            lname = data['LastName']
+        except:
+            lname = "N/A"
+
+        print("MPESA DATA RECEIEVED: ",data)
+
+        ctob_obj = CtoBop(trans_id,trans_time,trans_amnt,trans_type,business_shortcode,bill_ref_num,invoice_num,msisdn,org_acc_bal,fname,lname)
+        ctob_obj.save()
+
+        msg = f"LAGAD MPESA DATA JUST IN {trans_amnt} from {fname}"
+        response = sms.send(msg, ["+254716674695"],"KIOTAPAY")
+
+        mpesa_response(ctob_obj)
+
 
 class CallBackUrlEquity(Resource):
     def get(self):
--- mpesa.py
+++ mpesa.py
@@ -13,8 +13,8 @@ def generate_access_token():
     # consumer_key = "710QNGdu7FWDsP0Xca9VIRI9DkGIWuCA"
     # consumer_secret = "dAoOC51LeAFTkQaa"
 
-    consumer_key = "963Y5TYldaRIM24OvdcLegt6v4iUGZov"
-    consumer_secret = "ihqTfDXoGAFdT2V9"
+    consumer_key = "dPTnQGcGJuq8w4NPddmY95ml7cdI4IYS"
+    consumer_secret = "wbNjo3FPNLnPNxbG"
 
     api_URL = "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
@@ -38,10 +38,10 @@ def register_url():
     headers = {"Authorization": "Bearer %s" % my_access_token}
 
     request = {
-        "ShortCode": "203027",
+        "ShortCode": "4091201",
         "ResponseType": "Completed",
-        "ConfirmationURL": "https://kiotapay.com/m/bizlinebestel/ins/payment",
-        "ValidationURL":   "https://kiotapay.com/m/bizlinebestel/validate",
+        "ConfirmationURL": "https://kiotapay.com/m/lagad/ins/payment",
+        "ValidationURL":   "https://kiotapay.com/m/lagad/validate",
     }
 
     try:

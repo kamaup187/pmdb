c2b09c8714795dae6a3e20b398602c6fcd85a832
Koech Peter <koechpetersn@gmail.com>
2022-08-17 20:36:17 +0300

    update

--- wsgi_do.py
+++ wsgi_do.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
-from app.v1.views.secrets import *
+from global_secrets import APP_SETTINGS
 # from dapp import create_dapp
 
 configuration = os.getenv('APP_SETTINGS') or APP_SETTINGS

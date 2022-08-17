db48bd53dceef85a75003bad3148cda95461336c
Koech Peter <koechpetersn@gmail.com>
2022-08-17 20:44:07 +0300

    update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-from app.v1.views.secrets import *
+from global_secrets import *
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
--- global_secrets.py
+++ global_secrets.py
@@ -1,1 +1,7 @@
+from .app.v1.views.secrets import DATABASE_URL, HEROKU_POSTGRESQL_AQUA_URL, TEST_DATABASE_URL
+
+
 APP_SETTINGS="production"
+HEROKU_POSTGRESQL_AQUA_URL="postgres://bbgozuzflysbpp:752252c232a69248fcaa434826b26dc492160fb8c3648c03f1396115d1ccbf70@ec2-3-221-58-65.compute-1.amazonaws.com:5432/d97c9989tu9isf"
+DATABASE_URL=""
+TEST_DATABASE_URL="postgresql:///testdb"

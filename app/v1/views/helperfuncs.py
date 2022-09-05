c7d5ed2b6f68f5749e2ec1ed0b6b5bedf5201b6b
Koech Peter <koechpetersn@gmail.com>
2022-09-05 14:04:27 +0300

    fix email issue

--- app/v1/views/helperfuncs.py
+++ app/v1/views/helperfuncs.py
@@ -4137,141 +4137,141 @@ def send_out_single_email_invoice(billid):
     app = create_app(configuration)
     app.app_context().push()
 
-    try:
-        bill = MonthlyChargeOp.fetch_specific_bill(billid)
+    # try:
+    bill = MonthlyChargeOp.fetch_specific_bill(billid)
 
-        print("BILL IS HERE >>>>",bill)
+    print("BILL IS HERE >>>>",bill)
 
-        with mail.connect() as conn:
+    with mail.connect() as conn:
 
-            if bill.ptenant:
-                tenant=bill.ptenant
-                current_target = "owner"
-                print("OWNER INVOICE IS BEING PREPARED FOR" ,bill.ptenant.name)
-            else:
-                current_target = "tenant"
-                tenant=bill.tenant
-                print("TENANT INVOICE IS BEING PREPARED FOR" ,bill.tenant.name)
+        if bill.ptenant:
+            tenant=bill.ptenant
+            current_target = "owner"
+            print("OWNER INVOICE IS BEING PREPARED FOR" ,bill.ptenant.name)
+        else:
+            current_target = "tenant"
+            tenant=bill.tenant
+            print("TENANT INVOICE IS BEING PREPARED FOR" ,bill.tenant.name)
 
-            co = bill.apartment.company
+        co = bill.apartment.company
+
+        invnum = bill.id + 13285
+
+        house = bill.house
+
+        #start here
+        sibling_water_bill = fetch_current_billing_period_readings(bill.apartment.billing_period,bill.house.meter_readings)
+        sibling_electricity_bill = fetch_current_billing_period_readings_alt(bill.apartment.billing_period,bill.house.meter_readings)
+
+        try:
+            wbill = sibling_water_bill[0]
+            w_edited = "dispnone" if wbill.units == float(wbill.reading) - float(wbill.last_reading) else ""
+        except:
+            wbill = None
+            w_edited = "dispnone"
+
+        try:
+            ebill = sibling_electricity_bill[0]
+            e_edited = "dispnone" if ebill.units == float(ebill.reading) - float(ebill.last_reading) else ""
+        except:
+            ebill = None
+            e_edited = "dispnone"
+
+
+        kiotapay = CompanyOp.fetch_company_by_name("KiotaPay")
+        invdate = bill.date - relativedelta(days = 0)
+        inv_date = invdate.strftime("%d/%b/%y")
+        invdue = invdate + relativedelta(days=5)
+        inv_due = invdue.strftime("%d/%b/%y")
+
     
-            invnum = bill.id + 13285
-
-            house = bill.house
-
-            #start here
-            sibling_water_bill = fetch_current_billing_period_readings(bill.apartment.billing_period,bill.house.meter_readings)
-            sibling_electricity_bill = fetch_current_billing_period_readings_alt(bill.apartment.billing_period,bill.house.meter_readings)
-
-            try:
-                wbill = sibling_water_bill[0]
-                w_edited = "dispnone" if wbill.units == float(wbill.reading) - float(wbill.last_reading) else ""
-            except:
-                wbill = None
-                w_edited = "dispnone"
-
-            try:
-                ebill = sibling_electricity_bill[0]
-                e_edited = "dispnone" if ebill.units == float(ebill.reading) - float(ebill.last_reading) else ""
-            except:
-                ebill = None
-                e_edited = "dispnone"
-
-
-            kiotapay = CompanyOp.fetch_company_by_name("KiotaPay")
-            invdate = bill.date - relativedelta(days = 0)
-            inv_date = invdate.strftime("%d/%b/%y")
-            invdue = invdate + relativedelta(days=5)
-            inv_due = invdue.strftime("%d/%b/%y")
-
-        
-            email_addr = tenant.email
-            if email_addr:
-                if wbill or ebill:
-                    visibility = ""
-                else:
-                    visibility = "hide"
-
-                arrears = bill.arrears
-                
-                if bill.paid_amount:
-                    billpaid = f"{bill.paid_amount:,.2f}"
-                    billbal = f"{bill.balance:,.2f}"
-
-                else:
-                    billpaid = 0.0
-                    billbal = 0.0
-
-                if arrears < 0.0:
-                    arrtitle = "Previous balance"
-                    bbfhighlight = "text-success"
-
-                    arrears = f"{arrears*-1}"
-                elif arrears > 0.0:
-                    arrtitle = "Previous balance"
-                    bbfhighlight = "text-danger"
-                else:
-                    arrtitle = ""
-                    bbfhighlight = ""
-
-                try:
-                    if current_target == "owner":
-                        if bill.house.watertarget == "owner":
-                            watertarget = True
-                        else:
-                            watertarget = False
-                    else:
-                        if bill.house.watertarget == "tenant":
-                            watertarget = True
-                        else:
-                            watertarget = False
-                except:
-                    watertarget = True
-
-                try:
-                    if bill.apartment.paymentdetails.nartype == 'hsenum':
-                        narration = bill.house.name
-                    else:
-                        if bill.ptenant:
-                            narration = "WN"+str(tenant.id)
-                        else:
-                            narration = "TNT"+str(tenant.id)
-                except:
-                    narration = bill.house.name
-
-                template_vars = {
-                    "bill":bill,
-                    "p":bill.apartment.paymentdetails,
-                    "narration":narration,
-                    "servicevisibility":"",
-                    "readings": wbill,
-                    "w_edited": w_edited,
-                    "ereadings": ebill,
-                    "e_edited": e_edited,
-                    "visibility":visibility,
-                    "watertarget":watertarget,
-                    "arrears":arrears,
-                    "bbfhighlight ": bbfhighlight,
-                    "arrtitle":arrtitle,
-                    "billpaid":billpaid,
-                    "billbal":billbal,
-                    "house":house,
-                    "total":f"{bill.total_bill:,.2f}",
-                    "invdate":inv_date,
-                    "invdue":inv_due,
-                    "client":tenant,
-                    "company":co,
-                    "invnum":invnum,
-                    "logo":logo(co)[2],
-                    "slogo":logo(kiotapay)[1]
-                }
-
-                mail_sender(conn,tenant,bill,template_vars,email_addr,co)
+        email_addr = tenant.email
+        if email_addr:
+            if wbill or ebill:
+                visibility = ""
             else:
-                print("Email address not found for tenant ",tenant.name,"-",bill.apartment.name)                             
+                visibility = "hide"
 
-    except Exception as e:
-        print("WORKING HAS STOPPED >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",e)
+            arrears = bill.arrears
+            
+            if bill.paid_amount:
+                billpaid = f"{bill.paid_amount:,.2f}"
+                billbal = f"{bill.balance:,.2f}"
+
+            else:
+                billpaid = 0.0
+                billbal = 0.0
+
+            if arrears < 0.0:
+                arrtitle = "Previous balance"
+                bbfhighlight = "text-success"
+
+                arrears = f"{arrears*-1}"
+            elif arrears > 0.0:
+                arrtitle = "Previous balance"
+                bbfhighlight = "text-danger"
+            else:
+                arrtitle = ""
+                bbfhighlight = ""
+
+            try:
+                if current_target == "owner":
+                    if bill.house.watertarget == "owner":
+                        watertarget = True
+                    else:
+                        watertarget = False
+                else:
+                    if bill.house.watertarget == "tenant":
+                        watertarget = True
+                    else:
+                        watertarget = False
+            except:
+                watertarget = True
+
+            try:
+                if bill.apartment.paymentdetails.nartype == 'hsenum':
+                    narration = bill.house.name
+                else:
+                    if bill.ptenant:
+                        narration = "WN"+str(tenant.id)
+                    else:
+                        narration = "TNT"+str(tenant.id)
+            except:
+                narration = bill.house.name
+
+            template_vars = {
+                "bill":bill,
+                "p":bill.apartment.paymentdetails,
+                "narration":narration,
+                "servicevisibility":"",
+                "readings": wbill,
+                "w_edited": w_edited,
+                "ereadings": ebill,
+                "e_edited": e_edited,
+                "visibility":visibility,
+                "watertarget":watertarget,
+                "arrears":arrears,
+                "bbfhighlight ": bbfhighlight,
+                "arrtitle":arrtitle,
+                "billpaid":billpaid,
+                "billbal":billbal,
+                "house":house,
+                "total":f"{bill.total_bill:,.2f}",
+                "invdate":inv_date,
+                "invdue":inv_due,
+                "client":tenant,
+                "company":co,
+                "invnum":invnum,
+                "logo":logo(co)[2],
+                "slogo":logo(kiotapay)[1]
+            }
+
+            mail_sender(conn,tenant,bill,template_vars,email_addr,co)
+        else:
+            print("Email address not found for tenant ",tenant.name,"-",bill.apartment.name)                             
+
+    # except Exception as e:
+    #     print("WORKING HAS STOPPED >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",e)
 
             
 

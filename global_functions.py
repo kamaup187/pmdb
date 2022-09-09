8c1dceaf4b6b9b679ebb12372470531262652105
Koech Peter <koechpetersn@gmail.com>
2022-09-09 20:40:12 +0300

    update payments process

--- global_functions.py
+++ global_functions.py
@@ -5,7 +5,7 @@ import datetime
 original_stdout = sys.stdout # Save a reference to the original standard output
 
 def lfile(*args):
-    with open('logfile.txt', 'a') as f:
+    with open('logfile2.txt', 'a') as f:
         sys.stdout = f # Change the standard output to the file we created.
         full_str = ""
         for i in locals()['args']:

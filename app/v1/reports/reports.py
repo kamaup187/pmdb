0db83120905b6e92410081e696807dbc29a64b87
Koech Peter <koechpetersn@gmail.com>
2022-10-14 12:32:17 +0300

    update

--- app/v1/reports/reports.py
+++ app/v1/reports/reports.py
@@ -6556,21 +6556,27 @@ class FetchPayments(Resource):
             propid = get_identifier(prop_id)
             prop = ApartmentOp.fetch_apartment_by_id(propid)
             sifted = []
-            if prop.payment_bank == "PayBill" or prop.paymentdetails.mpesapaybill:
-                shortcode = prop.payment_bankacc
-                if not shortcode:
-                    shortcode = prop.paymentdetails.mpesapaybill
-                raw_unclaimed = CtoBop.fetch_all_records_by_shortcode(shortcode)
+            try:
+                if prop.payment_bank == "PayBill" or prop.paymentdetails.mpesapaybill:
+                    shortcode = prop.payment_bankacc
+                    if not shortcode:
+                        shortcode = prop.paymentdetails.mpesapaybill
+                    if shortcode == "000000":
+                        raw_unclaimed = prop.company.cbids
+                    else:
+                        raw_unclaimed = CtoBop.fetch_all_records_by_shortcode(shortcode)
 
-                for r in raw_unclaimed:
-                    # targets = ["532406","964399","4012401","4081687"]
-                    # if r.post_date.day == 6 and r.post_date.year == 2022 and r.post_date.month == 4:
-                    #     pass
-                    # else:
-                    #     if r.business_shortcode in targets:
-                    #         CtoBop.update_status(r,"claimed")
-                    if r.status == "unclaimed":
-                        sifted.append(r)
+                    for r in raw_unclaimed:
+                        # targets = ["532406","964399","4012401","4081687"]
+                        # if r.post_date.day == 6 and r.post_date.year == 2022 and r.post_date.month == 4:
+                        #     pass
+                        # else:
+                        #     if r.business_shortcode in targets:
+                        #         CtoBop.update_status(r,"claimed")
+                        if r.status == "unclaimed":
+                            sifted.append(r)
+            except:
+                pass
 
             unclaimed = ctb_payment_details(sifted)
             cbids = get_obj_ids(unclaimed)

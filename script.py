7bef11b4cc07479236a7283990b4302b376752d6
koechpetersn <koechpetersn@gmail.com>
2022-12-15 16:55:02 +0300

    update

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from app.v1.views.helperfuncs import account_validation,account_pesalink_transfer,ins_api_account_subscription
+from app.v1.views.helperfuncs import account_validation,account_pesalink_transfer,ins_api_account_subscription, generate_hash
 
 testkey = "IlcPPVkyc0mKe3jeufo9bjII6Mwa"
 testsec = "Def5yXFOSgKNJAzJshxDE8YkvSca"
@@ -14,4 +14,6 @@ prodsec = "CEMIEl7b2IMn8NlTlg7qVcY74Joa"
 ins_api_account_subscription(prodkey,prodsec,"01148173864900","622521Pt#")
 
 # account_pesalink_transfer(prodkey,prodsec,"01148173864900","01148173864901")
+
+# print(generate_hash("merit","q150c2bf1c4ee7da42yt"))
 

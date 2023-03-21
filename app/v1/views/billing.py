6df68af70576374bda14e28776b6038e28f042af
Koech Peter <koechpetersn@gmail.com>
2023-03-21 17:44:13 +0300

    update

--- app/v1/views/billing.py
+++ app/v1/views/billing.py
@@ -3479,6 +3479,40 @@ class UpdateBalance(Resource):
 
         return (f"{bill_balance:,.1f} ")
 
+class UpdateDeposit(Resource):
+    @login_required
+    def get(self):
+        tenant_id = request.args.get("tenantid")
+        ttype = request.args.get("ttype")
+
+        if ttype == "owner" or ttype == "resident":
+            return ""
+        else:
+            tenant_obj = TenantOp.fetch_tenant_by_id(tenant_id)
+            house_obj = check_house_occupied(tenant_obj)[1]
+
+        dep = tenant_obj.deposits
+        if not dep:
+            try:
+                dt = check_house_occupied(tenant_obj)[2].checkin_date
+            except:
+                dt = tenant_obj.date
+            
+            if house_obj.housecode:
+                status = "unrefunded"
+                rentdep = house_obj.housecode.rentrate if house_obj.housecode.rentrate else 0.0
+                waterdep = house_obj.housecode.waterdep if house_obj.housecode.waterdep else 0.0
+                elecdep = house_obj.housecode.elecdep if house_obj.housecode.elecdep else 0.0
+
+                total = rentdep+waterdep+elecdep
+
+                print("CREATING tenant deposits...for >>",house_obj, "total: ", total, "STATUS: ", status)
+                dep = TenantDepositOp(rentdep,waterdep,elecdep,0.0,total,dt,status,tenant_obj.id,None,house_obj.id,house_obj.apartment_id)
+                dep.save()
+                TenantOp.update_deposit(tenant_obj,total)
+
+        return tenant_obj.deposit
+
 class Receipt(Resource):
     @login_required
     def get(self):

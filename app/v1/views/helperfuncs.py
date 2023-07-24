ae9a4d11183264cab4a1cbcda666523d5dd77c77
Koech Peter <koechpetersn@gmail.com>
2023-07-24 18:05:49 +0300

    update

--- app/v1/views/helperfuncs.py
+++ app/v1/views/helperfuncs.py
@@ -17,6 +17,7 @@ from apiclient import discovery
 # from multiprocessing import allow_connection_pickling
 import os
 import base64
+import hashlib
 # from pickle import TRUE
 # from africastalking import initialize
 
@@ -201,6 +202,97 @@ def get_leads_api(current_user):
         print("Service was not found")
 
         return [0,0]
+
+
+
+
+class TokenGen(object):
+    """
+    Base class of every token generator.
+    """
+
+    def __init__(self):
+        """
+        Create a new instance of a token generator.
+        """
+        self.expires_in = {}
+        self.refresh_expires_in = 0
+
+    def create_access_token_data(self, data, grant_type, user_id):
+            """
+            Create data needed by an access token.
+
+            :param data: Arbitrary data as returned by the ``authenticate()`` method of a ``SiteAdapter``.
+            :type data: dict
+            :param grant_type:
+            :type grant_type: str
+            :param user_id: Identifier of the current user as returned by the ``authenticate()`` method of a ``SiteAdapter``.
+            :type user_id: int
+
+            :return: A ``dict`` containing the ``access_token`` and the
+                    ``token_type``. If the value of ``TokenGenerator.expires_in``
+                    is larger than 0, a ``refresh_token`` will be generated too.
+            :rtype: dict
+
+            .. versionchanged:: 1.1.0
+                New parameters ``data`` and ``user_id``
+            """
+            result = {"access_token": self.generate(data, user_id), "token_type": "Bearer"}
+
+            if self.expires_in.get(grant_type, 0) > 0:
+                result["refresh_token"] = self.generate()
+
+                result["expires_in"] = self.expires_in[grant_type]
+
+            return result
+
+
+    def generate(self, data=None, user_id=None):
+            """
+            Implemented by generators extending this base class.
+
+            :param data: Arbitrary data as returned by the ``authenticate()`` method of a ``SiteAdapter``.
+            :type data: dict
+            :param user_id: Identifier of the current user as returned by the ``authenticate()`` method of a ``SiteAdapter``.
+            :type user_id: int
+
+            :raises NotImplementedError:
+
+            .. versionchanged:: 1.1.0
+                New parameters ``data`` and ``user_id``
+            """
+            raise NotImplementedError
+    
+
+class GenerateToken(TokenGen):
+    """
+    Create a token using ``os.urandom()``.
+    """
+
+    def __init__(self, length=40):
+        self.token_length = length
+        TokenGen.__init__(self)
+
+    def generate(self, data=None, company_id=None):
+        """
+        :return: A new token
+        :rtype: str
+        """
+        passphrase = data['ckey'] + ':' + data['skey']
+        encoded_p = base64.b64encode(passphrase.encode()).decode()
+        return encoded_p
+
+
+def get_token(data=None, company_id=None):
+    # a =GenerateToken()
+    malibu = {
+        "ckey":"malibu",
+        "skey":"malibu"
+    }
+    a=GenerateToken()
+    b = GenerateToken.create_access_token_data(a,malibu,"client_credentials",1)
+    print("",b)
+    return b
 
 def myprint(txt):
     

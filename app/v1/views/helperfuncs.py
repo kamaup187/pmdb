3620b3be626f36081b9c7c707430cf3716901df6
Peter Griffin <koechpetersn@gmail.com>
2024-04-08 20:59:22 +0300

    update

--- app/v1/views/helperfuncs.py
+++ app/v1/views/helperfuncs.py
@@ -3206,7 +3206,8 @@ def new_tenants_injector_alt(apartment_id):
     tenants = filter_in_tenant_allocs(apartment_id)
     may_epoch = generate_exact_date(27,5,2023)
     for alloc in tenants:
-        if alloc.tenant.residency == "New" and alloc.date > may_epoch:
+        # if alloc.tenant.residency == "New" and alloc.date > may_epoch:
+        if alloc.tenant.residency == "New":
             new_list.append(alloc.tenant)
     return new_list
 

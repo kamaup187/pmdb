a8087c73a22e0883fa751232a46c02c3d3746a51
Koech Peter <koechpetersn@gmail.com>
2024-04-10 20:11:08 +0300

    update

--- app/v1/views/apartment_management.py
+++ app/v1/views/apartment_management.py
@@ -6604,7 +6604,7 @@ class AddTenant(Resource):
                     if bool_migrate:
                         TenantOp.update_residency(tenant_obj,"New")
                     else:
-                        TenantOp.update_residency(tenant_obj,"New")
+                        TenantOp.update_residency(tenant_obj,"Old")
             
             #     msg = "Client added successfully"
             # return msg + proceed
